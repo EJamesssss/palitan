dd7f140b5f9f0822a821d71853c5bfe46fd943de
EJamesssss <ejames.hegina@gmail.com>
2022-06-30 09:22:15 +0800

    redirect to homepage if not admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    confirmations: 'confirmations'
-  }
   resources :dashboard, only: [:index]
   resources :home, only: [:index]
   devise_scope :user do
     root to: "devise/sessions#new"
   end
+
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
