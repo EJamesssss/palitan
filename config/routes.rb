bf73d0f137e15cfe2cadb472f0b3cd5f110b4efa
EJamesssss <ejames.hegina@gmail.com>
2022-07-01 14:29:40 +0800

    fixed routings on dashboard and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  get '/dashboard/pending_users', to: 'dashboard#pending_users', as: 'pending_users'
   resources :dashboard
-  get '/dashboard/pending_users', to: 'dashboard#pending_users'
   resources :home, only: [:index]
   devise_scope :user do
     root to: "devise/sessions#new"
   end
-  resources :users, only: [:show]
-
   devise_for :users
+  resources :users
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
