ee0a2029cdd60d801db11ed41655d709ca0f3386
Danica Limon <danica.limon1996@gmail.com>
2022-07-06 13:54:36 +0800

    added a form for adding a portfolio

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,10 +1,9 @@
 class PortfolioController < ApplicationController
     def index
-        @portfolios = current_user.portfolios
+      @portfolios = current_user.portfolios
     end 
     
     private
-  
     def set_user
       @user = current_user
     end
