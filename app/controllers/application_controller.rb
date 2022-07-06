a43162f501180910365d7fa3d821eb408f1813c4
EJamesssss <ejames.hegina@gmail.com>
2022-07-06 10:20:37 +0800

    setup iex credentials and insert Client in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
 
+    Client = IEX::Api::Client.new
+
     private
         def after_sign_in_path_for(resource)
             home_index_path
         end
 
-        def iex_resource
-            @client = IEX::API::Client.new
-        end
 end
