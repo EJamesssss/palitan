1fd7c67066f49a0f3ed3b9ef611289681a1e6100
Danica Limon <danica.limon1996@gmail.com>
2022-07-06 20:42:50 +0800

    fix bug in searching portfolio

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -2,9 +2,4 @@ class PortfolioController < ApplicationController
     def index
       @portfolios = current_user.portfolios
     end 
-    
-    private
-    def set_user
-      @user = current_user
-    end
 end
