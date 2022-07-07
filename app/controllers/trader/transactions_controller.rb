3da07ac193a4f3fcafd45679083288a1a0a69eae
Danica Limon <danica.limon1996@gmail.com>
2022-07-07 12:43:32 +0800

    added a create method in transaction controller

--- app/controllers/trader/transactions_controller.rb
+++ app/controllers/trader/transactions_controller.rb
@@ -5,19 +5,32 @@ class Trader::TransactionsController < ApplicationController
   end
 
   def show
-    @price = @client.quote(params[:logo])
-    @company = @client.company(params[:logo])
-    @logo = @client.logo(params[:logo])
+
   end
 
   def new
+    @price = @client.quote(params[:symbol])
+    @company = @client.company(params[:symbol])
+    @logo = @client.logo(params[:symbol])
+    @transaction = current_user.transactions.build
   end
 
   def create
+    @transaction = current_user.transactions.build(transaction_params)
+    byebug
+    if @transaction.save
+      redirect_to portfolio_index_path
+    else
+      render :new
+    end
   end
 
   private
   def initialize_iex_client
     @client = IEX::Api::Client.new
+  end
+
+  def transaction_params
+    params.require(:transaction).permit(:user_id, :symbol, :company_name, :shares, :cost_price, :total, :transaction_type)
   end
 end
